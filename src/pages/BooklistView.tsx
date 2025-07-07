91306e7d3519a707cffc51813f7d74084993329e
Michael Swain <michaelswain@tommy.com>
2025-07-07 10:31:34 +0200

    create Modal component

--- src/pages/BooklistView.tsx
+++ src/pages/BooklistView.tsx
@@ -16,6 +16,7 @@ import { Toolbar } from "../components/Toolbar/Toolbar.tsx";
 import { SortControls } from "../components/SortControls/SortControls.tsx";
 import { Button } from "../components/Button/Button.tsx";
 import { PageHeader } from "../components/PageHeader/PageHeader.tsx";
+import { Modal } from "../components/Modal/Modal.tsx";
 
 const portalElem = document.getElementById('portal') as HTMLElement;
 
@@ -46,13 +47,12 @@ export const BookListView = () => {
 
     const handleSubmit = (result: ResponseObject) => {
         if (result.success) {
-            console.log("SUCCESS");
             // re-fetch books
             getBooks();
             uiDrawerRef.current?.close();
         } else {
-            console.log("ERROR");
             // display error message
+            uiDrawerRef.current?.close();
             setIsShowingError(true);
         }
     }
@@ -138,13 +138,10 @@ export const BookListView = () => {
             }
 
             {isShowingError && 
-                <div data-testId="error-message" className="error-message">
-                    <Button onClick={() => setIsShowingError(false)} variant="ghost" type="button">
-                        <svg width="0.7857142857142857em" height="1em" viewBox="0 0 11 14" fill="none" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" focusable="false"><path d="M5.25 6.14844L8.85938 2.51172L9.48828 3.14062L5.85156 6.75L9.48828 10.3867L8.85938 11.0156L5.25 7.37891L1.61328 11.0156L0.984375 10.3867L4.62109 6.75L0.984375 3.14062L1.61328 2.51172L5.25 6.14844Z" fill="#1B1D1F"></path></svg>
-                    </Button>
-                    <div><p>Error!</p>
-                    </div>
-                </div>}
+                createPortal(
+                <Modal closeFunc={() => setIsShowingError(false)} title="Oh no!" message="Encountered an unexpected error!"></Modal>, 
+                portalElem)
+            }
         </>
     )
 }

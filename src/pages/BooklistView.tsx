49998f71e8973145faeb1099022ba7bf7a31ce83
Michael Swain <michaelswain@tommy.com>
2025-07-07 10:58:17 +0200

    set up useBookList hook and move logic from BookListView into hook

--- src/pages/BooklistView.tsx
+++ src/pages/BooklistView.tsx
@@ -1,6 +1,4 @@
-import { useEffect, useRef, useState, type ChangeEvent } from "react"
-import { fetchBooks, filterBooks, sortBooks } from "../utils/intro";
-import type { Book, ResponseObject, SortDirection, UIDrawerHandle } from "../types/index";
+import { useState } from "react"
 import { BookList } from "../components/BookList/BookList";
 import { ClickAwayListener } from "../components/ClickAwayListener/ClickAwayListener.tsx";
 import { Tooltip } from "../components/Tooltip/Tooltip.tsx";
@@ -10,93 +8,36 @@ import { UIDrawer } from "../components/UIDrawer/UIDrawer.tsx";
 import { BookForm } from "../components/BookForm/BookForm.tsx";
 import { TooltipItem } from "../components/TooltipItem/TooltipItem.tsx";
 import { LoadingView } from "../components/LoadingView/LoadingView.tsx";
-import { useLoading } from "../hooks/useLoading/useLoading.ts";
 import { SearchInput } from "../components/SearchInput/SearchInput.tsx";
 import { Toolbar } from "../components/Toolbar/Toolbar.tsx";
 import { SortControls } from "../components/SortControls/SortControls.tsx";
 import { Button } from "../components/Button/Button.tsx";
 import { PageHeader } from "../components/PageHeader/PageHeader.tsx";
 import { Modal } from "../components/Modal/Modal.tsx";
+import { useBookList } from "../hooks/useBookList.ts";
 
 const portalElem = document.getElementById('portal') as HTMLElement;
 
 export const BookListView = () => {
-    const [bookList, setBookList] = useState<Array<Book>>([]);
-    const [filteredBookList, setFilteredBookList] = useState<Array<Book>>([]);
-    const [isShowingError, setIsShowingError] = useState<Boolean>(false);
-    const [isShowingForm, setIsShowingForm] = useState<Boolean>(false);
-    const [isShowingTooltip, setIsShowingTooltip] = useState<Boolean>(false);
-    const [sortOption, setSortOption] = useState<string>("title");
-    const [sortDirection, setSortDirection] = useState<SortDirection>("asc");
-    const { isLoading, withLoading } = useLoading();
-    const uiDrawerRef = useRef<UIDrawerHandle>(null);
+    const {
+        filteredBookList,
+        sortOption,
+        sortDirection,
+        isLoading,
+        isShowingError,
+        uiDrawerRef,
+        isShowingTooltip,
+        setIsShowingTooltip,
+        handleSearchInputChange,
+        handleSubmit,
+        handleSortOption,
+        handleSortDirection,
+        onDelete,
+        onUpdate,
+        setIsShowingError
+    } = useBookList();
 
-    const getBooks = async () => {
-        const { data: books, success } = await withLoading(fetchBooks);
-
-        if (success && books && Array.isArray(books)) {
-            setBookList(books);
-            setFilteredBookList(books);
-        }
-    }
-
-    const handleSearchInputChange = (inputText: string) => {
-        const filtered = filterBooks(bookList, inputText);
-        setFilteredBookList(filtered);
-    }
-
-    const handleSubmit = (result: ResponseObject) => {
-        if (result.success) {
-            // re-fetch books
-            getBooks();
-            uiDrawerRef.current?.close();
-        } else {
-            // display error message
-            uiDrawerRef.current?.close();
-            setIsShowingError(true);
-        }
-    }
-
-    const handleSortBooks = () => {
-        const sortedBooks = sortBooks(filteredBookList, sortOption, sortDirection);
-        setFilteredBookList(sortedBooks);
-    }
-
-    const handleSortOption = (e: React.MouseEvent<HTMLLIElement>) => {
-        const sortOption = e.currentTarget.textContent?.toLowerCase();
-        if (sortOption) {
-            setSortOption(sortOption);
-            setIsShowingTooltip(false);
-        } else {
-            // handle error...
-        }
-    }
-
-    const handleSortDirection = () => {
-        const newSortDirection = sortDirection === "desc" ? "asc" : "desc";
-        setSortDirection(newSortDirection);
-    }
-
-    const onDelete = (result: ResponseObject) => {
-        if (result.success) {
-            getBooks();
-        }
-    }
-
-    const onUpdate = (result: ResponseObject) => {
-        if (result.success) {
-            getBooks();
-        }
-    }
-
-    useEffect(() => {
-        getBooks();
-    }, []);
-
-
-    useEffect(() => {
-        handleSortBooks();
-    }, [sortOption, sortDirection]);
+    const [isShowingForm, setIsShowingForm] = useState(false);
 
     return (
         <>

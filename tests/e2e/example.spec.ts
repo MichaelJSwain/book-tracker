b672d29212de3ceb7b4a3b2d554072e3d99aa16f
Michael Swain <michaelswain@tommy.com>
2025-07-09 16:00:49 +0200

    check metadata and elements for empty view

--- tests/e2e/example.spec.ts
+++ tests/e2e/example.spec.ts
@@ -5,9 +5,15 @@ test.describe("Book List Page", () => {
     await page.goto("http://localhost:5173");
   });
 
-  test("should have correct metadata and elements", async ({page}) => {
+  test("should have correct metadata and elements (empty view)", async ({page}) => {
+    const title = "Get Started!";
+    const message = "Click the 'Add books' button to get started.";
+
     await expect(page.getByRole('heading', { name: 'My Books' })).toBeVisible();
     await expect(page.getByRole('button', {name: 'Add book'})).toBeVisible();
-    await expect(page.getByPlaceholder('Search for a book...')).toBeVisible()
+    await expect(page.locator('.empty-view')).toBeVisible();
+    await expect(page.locator('.empty-view-img')).toBeVisible();
+    await expect(page.locator('.empty-view-title')).toHaveText(title);
+    await expect(page.locator('.empty-view-message')).toHaveText(message);
   });
 });

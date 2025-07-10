56393e2c294715ba564dab7eed13e5517fe42317
Michael Swain <michaelswain@tommy.com>
2025-07-10 16:13:01 +0200

    write test for deleting a book

--- tests/e2e/example.spec.ts
+++ tests/e2e/example.spec.ts
@@ -1,5 +1,29 @@
 import { test, expect } from '@playwright/test';
 
+const mockBooks = [
+  {
+    id: '1',
+    title: '1984',
+    author: 'George Orwell',
+    status: 'Reading',
+    imageUrl: 'https://m.media-amazon.com/images/I/71wANojhEKL._UF894,1000_QL80_.jpg'
+  },
+  {
+    id: '2',
+    title: 'The Hobbit',
+    author: 'J.R.R. Tolkien',
+    status: 'To Read',
+    imageUrl: 'https://m.media-amazon.com/images/I/712cDO7d73L._UF894,1000_QL80_.jpg'
+  },
+  {
+    id: '3',
+    title: 'Dune',
+    author: 'Frank Herbert',
+    status: 'Read',
+    imageUrl: 'https://m.media-amazon.com/images/I/81Ua99CURsL.jpg'
+  }
+];
+
 test.describe("Book List Page", () => {
   test("should have correct metadata and elements (empty view)", async ({page}) => {
     const title = "Get Started!";
@@ -16,30 +40,6 @@ test.describe("Book List Page", () => {
   });
 
   test("should have correct metadata and elements (non-empty view)", async ({ page }) => {
-      const mockBooks = [
-        {
-          id: '1',
-          title: '1984',
-          author: 'George Orwell',
-          status: 'Reading',
-          imageUrl: 'https://m.media-amazon.com/images/I/71wANojhEKL._UF894,1000_QL80_.jpg'
-        },
-        {
-          id: '2',
-          title: 'The Hobbit',
-          author: 'J.R.R. Tolkien',
-          status: 'To Read',
-          imageUrl: 'https://m.media-amazon.com/images/I/712cDO7d73L._UF894,1000_QL80_.jpg'
-        },
-        {
-          id: '3',
-          title: 'Dune',
-          author: 'Frank Herbert',
-          status: 'Read',
-          imageUrl: 'https://m.media-amazon.com/images/I/81Ua99CURsL.jpg'
-        }
-      ];
-
       // Add the localStorage data before the page loads
       await page.addInitScript((books) => {
         localStorage.setItem('book_list', JSON.stringify(books));
@@ -83,5 +83,39 @@ test.describe("Book List Page", () => {
     await expect(page.locator('.status-label-icon [data-testid="icon-utility-wishlist-svg"]')).toBeVisible();
     await expect(page.locator('.status-label-text')).toHaveText("To Read");
     await expect(page.locator('.book-details')).toHaveText('Strange PicturesUketsu');
+  });
+
+  test("should delete a book", async ({ page }) => {
+
+
+      // Add the localStorage data before the page loads
+      await page.addInitScript((books) => {
+        localStorage.setItem('book_list', JSON.stringify(books));
+      }, mockBooks);
+
+      await page.goto('http://localhost:5173');
+
+      await page.locator('.book-card').count() === 3;
+      await page.locator('.book-card').first().getByRole('button', {name: "..."}).click();
+      await page.locator('.tooltip-item').nth(1).click();
+
+      await expect(page.locator('.book-card')).toHaveCount(2);
+      const firstBook = page.locator('.book-card').first()
+      await expect(firstBook.locator('.book-details')).not.toHaveText(mockBooks[0].title + mockBooks[0].author);
+      await expect(firstBook.locator('.book-img').getAttribute('src')).not.toBe(mockBooks[0].imageUrl);
+      await expect(firstBook.locator('.status-label-text')).not.toBe(mockBooks[0].status);
+
+      const pageDetails = await page.evaluate(() => {
+         const bookList = JSON.parse(localStorage.getItem("book_list") || '[]');
+        const isBookDeleted = !bookList.find(book => book.title === '1984');
+
+          return {
+            bookCount: bookList.length,
+            isBookDeleted
+          }
+      });
+
+      expect(pageDetails.bookCount).toBe(2);
+      expect(pageDetails.isBookDeleted).toBeTruthy();
   });
 });

95f5a8052d8e5bfcfe01d4700f0c0d7341893a11
Michael Swain <michaelswain@tommy.com>
2025-07-10 16:15:57 +0200

    move mockBooks to mockData file

--- tests/e2e/example.spec.ts
+++ tests/e2e/example.spec.ts
@@ -1,28 +1,5 @@
 import { test, expect } from '@playwright/test';
-
-const mockBooks = [
-  {
-    id: '1',
-    title: '1984',
-    author: 'George Orwell',
-    status: 'Reading',
-    imageUrl: 'https://m.media-amazon.com/images/I/71wANojhEKL._UF894,1000_QL80_.jpg'
-  },
-  {
-    id: '2',
-    title: 'The Hobbit',
-    author: 'J.R.R. Tolkien',
-    status: 'To Read',
-    imageUrl: 'https://m.media-amazon.com/images/I/712cDO7d73L._UF894,1000_QL80_.jpg'
-  },
-  {
-    id: '3',
-    title: 'Dune',
-    author: 'Frank Herbert',
-    status: 'Read',
-    imageUrl: 'https://m.media-amazon.com/images/I/81Ua99CURsL.jpg'
-  }
-];
+import { mockBooks } from '../mocks/mockData';
 
 test.describe("Book List Page", () => {
   test("should have correct metadata and elements (empty view)", async ({page}) => {

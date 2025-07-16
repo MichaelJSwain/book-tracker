ef58c394d4d231a70c449b20cc9d383114827b31
Michael Swain <michaelswain@tommy.com>
2025-07-16 16:26:16 +0200

    disable react in scope error in eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -5,7 +5,10 @@ import { defineConfig } from "eslint/config";
 
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"], languageOptions: { globals: globals.browser } },
+  { files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"], languageOptions: { globals: globals.browser },  rules:{
+     "react/jsx-uses-react": "off",
+     "react/react-in-jsx-scope": "off",
+  } },
   tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
 ]);

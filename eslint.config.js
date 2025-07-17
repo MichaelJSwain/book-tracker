7fc9d3ba98c98985e0d7a782515201ea34873e31
Michael Swain <michaelswain@tommy.com>
2025-07-17 09:14:31 +0200

    reorder eslint.config file

--- eslint.config.js
+++ eslint.config.js
@@ -3,14 +3,21 @@ import tseslint from "typescript-eslint";
 import pluginReact from "eslint-plugin-react";
 import { defineConfig } from "eslint/config";
 
-
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"], 
-    languageOptions: { globals: globals.browser },
-    rules: {
-      'react/react-in-jsx-scope': 'off',
-    }
-  },
   tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
+  { files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"], 
+    languageOptions: { globals: globals.browser },
+    plugins: {
+      react: pluginReact,
+    },
+    rules: {
+      'react/react-in-jsx-scope': 'off',
+    },
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+  },
 ]);
